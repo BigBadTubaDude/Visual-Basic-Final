15d43585640259b17fd1d8e6a2c0f2c18c8fb312
BigBadTubaDude <cralex0307@gmail.com>
2022-07-25 16:44:50 -0400

    first

---
+++ Shopping Cart/Shopping Cart/Main Form.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblShipping = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lstCart = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnClearDVDs = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnClearCart = New System.Windows.Forms.Button()
+        Me.lstDVDs = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(572, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(504, 241)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(47, 20)
+        Me.lblTotal.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(443, 241)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Total:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(443, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Shipping:"
+        '
+        'lblShipping
+        '
+        Me.lblShipping.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblShipping.Location = New System.Drawing.Point(504, 215)
+        Me.lblShipping.Name = "lblShipping"
+        Me.lblShipping.Size = New System.Drawing.Size(47, 20)
+        Me.lblShipping.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(443, 189)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 15)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Tax:"
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTax.Location = New System.Drawing.Point(504, 189)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(47, 20)
+        Me.lblTax.TabIndex = 6
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSubtotal.Location = New System.Drawing.Point(504, 163)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(47, 20)
+        Me.lblSubtotal.TabIndex = 7
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(443, 163)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(54, 15)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Subtotal:"
+        '
+        'lstCart
+        '
+        Me.lstCart.Font = New System.Drawing.Font("Courier New", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstCart.FormattingEnabled = True
+        Me.lstCart.ItemHeight = 15
+        Me.lstCart.Location = New System.Drawing.Point(339, 44)
+        Me.lstCart.Name = "lstCart"
+        Me.lstCart.Size = New System.Drawing.Size(220, 94)
+        Me.lstCart.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 15)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "DVDs:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(336, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 15)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Cart:"
+        '
+        'btnClearDVDs
+        '
+        Me.btnClearDVDs.Location = New System.Drawing.Point(15, 144)
+        Me.btnClearDVDs.Name = "btnClearDVDs"
+        Me.btnClearDVDs.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearDVDs.TabIndex = 13
+        Me.btnClearDVDs.Text = "&Clear"
+        Me.btnClearDVDs.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(249, 66)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 14
+        Me.btnAdd.Text = "&Add ->"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(249, 104)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 15
+        Me.btnRemove.Text = "<- &Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnClearCart
+        '
+        Me.btnClearCart.Location = New System.Drawing.Point(339, 144)
+        Me.btnClearCart.Name = "btnClearCart"
+        Me.btnClearCart.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearCart.TabIndex = 16
+        Me.btnClearCart.Text = "C&lear"
+        Me.btnClearCart.UseVisualStyleBackColor = True
+        '
+        'lstDVDs
+        '
+        Me.lstDVDs.Font = New System.Drawing.Font("Courier New", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDVDs.FormattingEnabled = True
+        Me.lstDVDs.ItemHeight = 15
+        Me.lstDVDs.Location = New System.Drawing.Point(15, 44)
+        Me.lstDVDs.Name = "lstDVDs"
+        Me.lstDVDs.Size = New System.Drawing.Size(220, 94)
+        Me.lstDVDs.Sorted = True
+        Me.lstDVDs.TabIndex = 17
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(572, 278)
+        Me.Controls.Add(Me.lstDVDs)
+        Me.Controls.Add(Me.btnClearCart)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnClearDVDs)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstCart)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblShipping)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Shopping Cart"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblShipping As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lstCart As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnClearDVDs As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnClearCart As Button
+    Friend WithEvents lstDVDs As ListBox
+End Class
---
+++ Shopping Cart/Shopping Cart/Main Form.vb
@@ -0,0 +1,27 @@
+﻿Public Class frmMain
+    Private DVD_FILE As String = "DVs.txt"
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ''''Variables
+        Dim inFile As IO.StreamReader
+        Dim DVDNames(9) As String
+        Dim DVDPrices(9) As Double
+        ''Tests that file exists, then moves contents to arrays and then to list boxes
+        If IO.File.Exists(DVD_FILE) Then
+            inFile = IO.File.OpenText(DVD_FILE)
+            Do Until inFile.Peek = -1
+                For i As Integer = 0 To 9
+                    DVDNames(i) = inFile.ReadLine
+                    DVDPrices(i) = inFile.ReadLine
+                Next i
+            Loop
+            For i As Integer = 0 To DVDPrices.Length - 1
+                lstDVDs.Items.Add(DVDNames(i).PadRight(20) & DVDPrices(i).ToString("C2").PadLeft(8))
+            Next i
+        Else 'If file does not exist, show error screen and exit
+            MessageBox.Show("Could not open " & DVD_FILE & "! Ending application.", "Shopping Cart", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            Me.Close()
+        End If
+    End Sub
+
+
+End Class
